4f189f83f46e9a13ae747a4e0ec8fa385514667c
agumaste14 <117681033+agumaste14@users.noreply.github.com>
2022-11-12 14:05:28 -0500

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -31,11 +31,11 @@ module ALU(
     case(alu_select)
         4'b0000: result = in_a + in_b; //ADD
         4'b0001: result = in_a - in_b; //SUB
-        4'b0010: result = in_a << in_b; //Shift Left Logical
+        4'b0010: result = in_a << in_b[4:0]; //Shift Left Logical
         4'b0011: result = $signed(in_a) < $signed(in_b); //Set Less than Signed
         4'b0100: result = $unsigned(in_a) < $unsigned(in_b); //Set Less than Unsigned
         4'b0101: result = in_a ^ in_b; //XOR
-        4'b0110: result = in_a >> in_b; //Shift Right Logical
+        4'b0110: result = in_a >> in_b[4:0]; //Shift Right Logical
         4'b0111: result = $signed(in_a) >>> in_b[4:0]; //Shift Right Arithmatic
         4'b1000: result = in_a | in_b; //OR
         4'b1001: result = in_a & in_b; //AND
