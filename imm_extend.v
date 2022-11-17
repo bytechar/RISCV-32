622e3fffc1edf24e2ad77ed83575e1f5365c553e
catchyviddy <40513877+catchyviddy@users.noreply.github.com>
2022-11-16 22:10:03 -0500

    Add files via upload

---
+++ imm_extend.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+`include "defines.vh"
+`default_nettype none
+
+module imm_extend(
+    input wire [31:0] instr,
+    output reg signed [31:0] immediate
+    );
+        
+    wire [6:0] opcode;
+    assign opcode = instr[6:0];
+    
+    always @(*) begin
+        case(opcode)
+            `AUIPC:  immediate = { instr[31:12]    , {12{1'b0}} };                                          //U-type
+            `JAL:    immediate = { {12{instr[31]}} , instr[19:12] , instr[20]    , instr[30:21] , 1'b0 };   //J-type
+            `JALR:   immediate = { {12{instr[31]}} , instr[19:12] , instr[20]    , instr[30:21] , 1'b0 };   //J-type
+            `BRANCH: immediate = { {20{instr[31]}} , instr[7]     , instr[30:25] , instr[11:8]  , 1'b0 };   //B-type
+            `STORE:  immediate = { {21{instr[31]}} , instr[30:25] , instr[11:7] };                          //S-type
+            `LOAD:   immediate = { {21{instr[31]}} , instr[30:20] };                                        //I-type
+            `IMM:    immediate = { {21{instr[31]}} , instr[30:20] };                                        //I-type
+            default: immediate = immediate;
+        endcase
+    end
+
+endmodule
