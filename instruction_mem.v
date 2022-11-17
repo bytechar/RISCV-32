3a27628c23b316c048e7920caf3ccceeddc6a500
catchyviddy <40513877+catchyviddy@users.noreply.github.com>
2022-11-16 21:19:06 -0500

    Update instruction_mem.v

--- instruction_mem.v
+++ instruction_mem.v
@@ -1,43 +1,27 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/07/2022 05:31:00 PM
-// Design Name: 
-// Module Name: instruction_mem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
+`include "defines.vh"
 `default_nettype none
+
 module instruction_mem(
     input wire clk,
     input wire [31:0] instr_addr,
     output wire [31:0] instr
     );
     
-    reg [7:0] imem ['h01000000:'h010007FF];
-    //initial begin
-    //    $readmemh("imem.mem", imem);
-    //end  
+    //define instruction memory from 0x01000000 to 0x010007FF
+    reg [31:0] imem ['h01000000/4:'h010007FC/4];
     
-    reg [31:0] instr_addr_reg;
+    //load instruction memory
+    initial $readmemh("imem.mem", imem);
+    
+    reg [31:2] instr_addr_reg;
     
     always@(posedge clk)
     begin
-        instr_addr_reg <= instr_addr;
+        instr_addr_reg <= instr_addr[31:2];
     end
     
-    assign instr = {imem[instr_addr_reg+3],imem[instr_addr_reg+2],imem[instr_addr_reg+1],imem[instr_addr_reg]};
+    //memory is byte addressed, however only entire word can be accessed at a time
+    assign instr = imem[instr_addr_reg];
     
 endmodule
