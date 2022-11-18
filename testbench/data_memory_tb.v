78af05d0ee7114d2f5f4a8309bc7964b21eb6204
Naman Sharma <64301687+naman-47@users.noreply.github.com>
2022-11-18 01:52:30 -0500

    Update data_memory_tb.v
    Removed Vivado comments

--- testbench/data_memory_tb.v
+++ testbench/data_memory_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/16/2022 08:07:33 PM
-// Design Name: 
-// Module Name: DMem_TB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 `define ANVAY 13874751
 `define NAMAN 18700095
