d29ce0dba775514d618bed0ae9f7fe23d4b67b28
Naman Sharma <64301687+naman-47@users.noreply.github.com>
2022-11-18 02:09:50 -0500

    Update data_extender.v
    corrected to blocking assignments

--- design/data_extender.v
+++ design/data_extender.v
@@ -12,17 +12,15 @@ output wire [31:0] d_out
     
     always@(*) begin
         case(opcode)
-            `LB: data_out <= {{24{d_in[7]}}, d_in[7:0]};
-            `LH: data_out <= {{16{d_in[15]}}, d_in[15:0]};
-            `LBU: data_out <= {{24{1'b0}}, d_in[7:0]};
-            `LHU: data_out <= {{16{1'b0}}, d_in[15:0]};
-            `LW: data_out <= d_in;
-            default: data_out <= 32'd0;
+            `LB: data_out = {{24{d_in[7]}}, d_in[7:0]};
+            `LH: data_out = {{16{d_in[15]}}, d_in[15:0]};
+            `LBU: data_out = {{24{1'b0}}, d_in[7:0]};
+            `LHU: data_out = {{16{1'b0}}, d_in[15:0]};
+            `LW: data_out = d_in;
+            default: data_out = 32'd0;
         endcase
     end
     
     assign d_out = data_out;
     
-endmodule
-
-
+endmodue
