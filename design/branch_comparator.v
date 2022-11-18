884f038318d56a74e9c10e4f268cf59223e6f79b
Naman Sharma <64301687+naman-47@users.noreply.github.com>
2022-11-18 02:40:54 -0500

    Update branch_comparator.v
    added default to remove rtl_latch

--- design/branch_comparator.v
+++ design/branch_comparator.v
@@ -6,24 +6,29 @@ module branch_comp(
     input wire [2:0] branch_op,
     input wire [31:0] data_in1,
     input wire [31:0] data_in2,
-    output reg branch_out
+    output wire branch_out
     );
+    
+    reg branch_output;
     
     always@(*) begin
         case(branch_op)
             `BEQ: // Branch if equal
-                branch_out = (data_in1==data_in2) ? 1:0;
+                branch_output = (data_in1==data_in2) ? 1:0;
             `BNE: // Branch if not equal
-                branch_out = (data_in1==data_in2) ? 0:1;
+                branch_output = (data_in1!=data_in2) ? 1:0;
             `BLT: // Branch if less than (signed)
-                branch_out = ($signed(data_in1) < $signed(data_in2)) ? 1:0;
+                branch_output = ($signed(data_in1) < $signed(data_in2)) ? 1:0;
             `BGE: // branch if greater than or equal (signed)
-                branch_out = ($signed(data_in1) < $signed(data_in2)) ? 0:1;
+                branch_output = ($signed(data_in1) > $signed(data_in2)) ? 1:0;
             `BLTU: // branch if less than (unsigned)
-                branch_out = (data_in1 < data_in2)? 1:0;
+                branch_output = (data_in1 < data_in2)? 1:0;
             `BGEU: // branch if greater than or equal (unsigned)
-                branch_out = (data_in1 < data_in2)? 0:1;
+                branch_output = (data_in1 > data_in2)? 1:0;
+             default: branch_output = 1'b0; 
         endcase
     end
+    
+    assign branch_out = branch_output;
     
 endmodule
