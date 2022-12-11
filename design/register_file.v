546c4147f415c6fe52ac1f407406d057b9201aa5
Vidyut <40513877+catchyviddy@users.noreply.github.com>
2022-12-11 01:57:43 -0500

    Update register_file.v
    updated rst to rstn

--- design/register_file.v
+++ design/register_file.v
@@ -3,7 +3,7 @@
 `default_nettype none
 
 module register_file(
-    input wire clk,rst,we,
+    input wire clk,rstn,we,
     input wire [4:0] rs1_addr, rs2_addr, rd_addr,
     input wire [31:0] rd_din,
     output wire [31:0] rs1_dout, rs2_dout
@@ -25,7 +25,7 @@ module register_file(
         rs2_addr_reg <= rs2_addr;
         
         //reset registers R1-R31 to 0, reset to R0 not permitted
-        if (rst==0) for(i=1;i<32;i=i+1) register_file[i] <= 32'h00000000;
+        if (rstn==0) for(i=1;i<32;i=i+1) register_file[i] <= 32'h00000000;
         
         //write to registers R1-R31 as per rd signal if write enable (we) is asserted
         //write to R0 is not permitted
