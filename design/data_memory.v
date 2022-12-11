b6aabf04cbf95ea81f036f980039dc34316bb5bd
Vidyut <40513877+catchyviddy@users.noreply.github.com>
2022-12-11 08:38:05 -0500

    Update data_memory.v
    updated special registers and block RAM model

--- design/data_memory.v
+++ design/data_memory.v
@@ -1,23 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/16/2022 01:02:42 PM
-// Design Name: 
-// Module Name: DataMemory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 `timescale 1ns / 1ps
 `default_nettype none
 `include "defines.vh"
@@ -41,13 +21,17 @@
 
 module DMem(
     input wire clk,
-    input wire rst,
+    input wire rstn,
     input wire rd,
     input wire [3:0] we,
+    input wire [2:0] load_select,
     input wire [31:0] addr_in,
     input wire [31:0] data_in,
-    output reg [31:0] data_out
+    output wire [31:0] data_out,
+    input wire [15:0] sw,
+    output reg [15:0] led
 );
+                  
     integer i;
     
     // Define Data Memory
@@ -57,56 +41,49 @@ module DMem(
     initial $readmemh("dmem.mem", ram);
  
     // Define Special Memory and IO Ports 
-    reg [31:0] rom [0:`ROM_LENGTH + `IO_LENGTH-1];
+    reg [31:0] rom [0:`ROM_LENGTH -1];
    
     // Loading Memory with hardcoded values
     initial begin
         // special read only registers for N numbers of group members
-        rom[0] <= 32'h`ANVAY;   //  when addr_in 0X00100000
+        rom[0] <= 32'h`ANVAY;   // when addr_in 0X00100000
         rom[1] <= 32'h`NAMAN;   // when addr_in 0X00100004
         rom[2] <= 32'h`VIDYUT;  // when addr_in 0X00100008
-       
-        // special read/write and read only I/O registers 
-        rom[3]<=32'd0; 
-        rom[4]<=32'd0;
     end
     
-       // Transalted address used with Dmem
+    // Transalted address used with Dmem
     wire [11:0] addr;
 
     // Address Translation divide by 4
     assign addr= {2'b00, addr_in[`RAM_ADDR_BITS-1:2]};
-
-    always @(posedge clk or negedge rst) begin  
     
-            if (rst == 0) begin
-            for (i=0; i<`RAM_LENGTH_WORDS; i = i+1) ram[i] <= 32'd0;
-            rom[4] <=0;
-            end
-            
-            else if (addr_in[`STARTING_ADDR_BIT-1]) begin
-                if (we)
-                    if(addr==12'd5) 
-                        rom[addr-1] <= data_in;                 // IO port write at addr == 4 (corresponds to rom[3])
-                if (rd)
-                    if(addr == 12'd4 || addr == 12'd5)        
-                        data_out <= rom[addr-1];                // IO port read allowed for special I/O registers (corresponds to rom[addr-1])
-                    else
-                        data_out <= rom[addr];                  // read N-Numbers from special read only registers
-            end
-            
-            else begin      
-                if (we[0])
-                    ram[addr][7:0] <= data_in[7:0];
-                if (we[1])
-                    ram[addr][15:8] <= data_in[15:8];                    
-                if (we[2])
-                    ram[addr][23:16] <= data_in[23:16];
-                if (we[3])
-                    ram[addr][31:24] <= data_in[31:24];
-                if (rd)
-                    data_out <= ram[addr];
-            end
+    // raw 32-bit data output from DMEM
+    reg [31:0] data_out_raw;
 
+    // data extender for raw 32-bit data output
+    data_ext DATA_EXT(.opcode(load_select), .d_in(data_out_raw), .d_out(data_out));
+
+    always @(posedge clk) begin  
+
+            if (rd) begin
+            	if (addr_in[`STARTING_ADDR_BIT-1]) begin
+           	        case(addr)
+           	            12'd0,12'd1,12'd2: data_out_raw <= rom[addr];      // read N-Numbers from special read only registers
+           	            12'd4: data_out_raw <= sw;                         // IO port read for switches
+           	            12'd5: data_out_raw <= led;                        // IO port read for leds
+           	            default: data_out_raw <= 32'b0;                    // send out 0 in case addr_in is out of bounda
+           	        endcase
+           	        if (we[0] && addr==12'd5) led[7:0] <= data_in[7:0];    // IO port write to leds
+           	        if (we[1] && addr==12'd5) led[15:8] <= data_in[15:8];
+	   	        end
+           	    else begin   
+           	        data_out_raw <= ram[addr];   
+           	        if (we[0]) ram[addr][7:0] <= data_in[7:0];
+           	        if (we[1]) ram[addr][15:8] <= data_in[15:8];                    
+           	        if (we[2]) ram[addr][23:16] <= data_in[23:16];
+           	        if (we[3]) ram[addr][31:24] <= data_in[31:24];
+		        end
+            end
     end
+
 endmodule
