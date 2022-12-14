86c46f5743b784e784f81362ee5a2d305d999291
Vidyut <40513877+catchyviddy@users.noreply.github.com>
2022-12-14 18:55:09 -0500

    Update data_memory.v
    Updated to support BRAM

--- design/data_memory.v
+++ design/data_memory.v
@@ -7,10 +7,11 @@
 
 // Relevant Address bits for 4KB Memory (0 - FFC -> 12 Bits)
 `define RAM_ADDR_BITS 12
+`define STARTING_ADDR_BIT 31
 
 //Special ROM Values
 `define ROM_LENGTH 3
-`define STARTING_ADDR_BIT 21
+`define ROM_ADDR_BIT 21
 
 `define ANVAY 13874751
 `define NAMAN 18700095
@@ -38,7 +39,7 @@ module DMem(
     reg [31:0] ram [0:`RAM_LENGTH_WORDS-1];
     
     // intialize Data Memory to 0
-    initial $readmemh("dmem.mem", ram);
+    initial $readmemh("dmem_int.mem", ram);
  
     // Define Special Memory and IO Ports 
     reg [31:0] rom [0:`ROM_LENGTH -1];
@@ -49,6 +50,7 @@ module DMem(
         rom[0] <= 32'h`ANVAY;   // when addr_in 0X00100000
         rom[1] <= 32'h`NAMAN;   // when addr_in 0X00100004
         rom[2] <= 32'h`VIDYUT;  // when addr_in 0X00100008
+        led <= 16'h0000;
     end
     
     // Transalted address used with Dmem
@@ -63,26 +65,41 @@ module DMem(
     // data extender for raw 32-bit data output
     data_ext DATA_EXT(.opcode(load_select), .d_in(data_out_raw), .d_out(data_out));
 
+    // pick data out from memory or other locations (mapped I/O or ROM)
+    reg [31:0] data_out_ram, data_out_rom;
+    
+    always@(*) begin
+        case({addr_in[`ROM_ADDR_BIT-1],addr_in[`STARTING_ADDR_BIT-1]})
+            2'b01: data_out_raw = data_out_ram;
+            2'b10: data_out_raw = data_out_rom;
+            default: data_out_raw = 32'h00000000;
+        endcase
+    end
+    
+    // we signals based on address
+    wire [3:0] we_ram;
+    wire [1:0] we_led;
+    
+    assign we_ram = addr_in[`STARTING_ADDR_BIT-1] ? we : 4'b0000 ;
+    assign we_led = (addr_in[`ROM_ADDR_BIT-1] && addr==12'd5) ? we[1:0] : 2'b00;
+
     always @(posedge clk) begin  
 
             if (rd) begin
-            	if (addr_in[`STARTING_ADDR_BIT-1]) begin
            	        case(addr)
-           	            12'd0,12'd1,12'd2: data_out_raw <= rom[addr];      // read N-Numbers from special read only registers
-           	            12'd4: data_out_raw <= sw;                         // IO port read for switches
-           	            12'd5: data_out_raw <= led;                        // IO port read for leds
-           	            default: data_out_raw <= 32'b0;                    // send out 0 in case addr_in is out of bounda
+           	            12'd0,12'd1,12'd2: data_out_rom <= rom[addr];      // read N-Numbers from special read only registers
+           	            12'd4: data_out_rom <= sw;                         // IO port read for switches
+           	            12'd5: data_out_rom <= led;                        // IO port read for leds
+           	            default: data_out_rom <= 32'b0;                    // send out 0 in case addr_in is out of bounda
            	        endcase
-           	        if (we[0] && addr==12'd5) led[7:0] <= data_in[7:0];    // IO port write to leds
-           	        if (we[1] && addr==12'd5) led[15:8] <= data_in[15:8];
-	   	        end
-           	    else begin   
-           	        data_out_raw <= ram[addr];   
-           	        if (we[0]) ram[addr][7:0] <= data_in[7:0];
-           	        if (we[1]) ram[addr][15:8] <= data_in[15:8];                    
-           	        if (we[2]) ram[addr][23:16] <= data_in[23:16];
-           	        if (we[3]) ram[addr][31:24] <= data_in[31:24];
-		        end
+           	        if (we_led[0]) led[7:0] <= data_in[7:0];    // IO port write to leds
+           	        if (we_led[1]) led[15:8] <= data_in[15:8];
+            
+           	        data_out_ram <= ram[addr];   
+           	        if (we_ram[0]) ram[addr][7:0] <= data_in[7:0];
+           	        if (we_ram[1]) ram[addr][15:8] <= data_in[15:8];                    
+           	        if (we_ram[2]) ram[addr][23:16] <= data_in[23:16];
+           	        if (we_ram[3]) ram[addr][31:24] <= data_in[31:24];
             end
     end
 
