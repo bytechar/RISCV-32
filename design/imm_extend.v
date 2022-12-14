bc2a18ec29e02b4de751c4a1a3e44f6fd1790233
Vidyut <40513877+catchyviddy@users.noreply.github.com>
2022-12-14 18:11:07 -0500

    Update imm_extend.v
    updated extend for LUI, LOAD, STORE

--- design/imm_extend.v
+++ design/imm_extend.v
@@ -14,12 +14,12 @@ module imm_extend(
     
     always @(*) begin
         case(opcode)
-            `AUIPC:  immediate = { instr[31:12]    , {12{1'b0}} };                                          //U-type
+       `AUIPC,`LUI:  immediate = { instr[31:12]    , {12{1'b0}} };                                          //U-type
             `JAL:    immediate = { {12{instr[31]}} , instr[19:12] , instr[20]    , instr[30:21] , 1'b0 };   //J-type
             `JALR:   immediate = { {21{instr[31]}} , instr[30:20] };                                        //I-type
             `BRANCH: immediate = { {20{instr[31]}} , instr[7]     , instr[30:25] , instr[11:8]  , 1'b0 };   //B-type
-            `STORE:  immediate = { {19{instr[31]}} , instr[30:25] , instr[11:7]  , {2{1'b0}} };             //S-type
-            `LOAD:   immediate = { {19{instr[31]}} , instr[30:20] , {2{1'b0}} };                            //L-type
+            `STORE:  immediate = { {21{instr[31]}} , instr[30:25] , instr[11:7]  };                         //S-type
+            `LOAD:   immediate = { {21{instr[31]}} , instr[30:20] };                                        //L-type
              //need to pick immediates differently for shift operations
             `IMM:    immediate = (funct3[0] && ~funct3[1]) ? { {27{1'b0}} , instr[24:20] } : { {21{instr[31]}} , instr[30:20] };
             default: immediate = { 32{1'b0} };
