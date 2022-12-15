2c76629c032d9657b8b54b82fae93659321166cd
Vidyut <40513877+catchyviddy@users.noreply.github.com>
2022-12-15 14:00:13 -0500

    Update instruction_mem.v
    reverting back to older version

--- design/instruction_mem.v
+++ design/instruction_mem.v
@@ -5,11 +5,9 @@
 module instruction_mem(
     input wire clk,rd,
     input wire [31:0] instr_addr,
-    output wire [31:0] instr
+    output reg [31:0] instr
     );
-    
-    reg [31:0] instr_temp;
-    
+        
     //define instruction memory from 0x01000000 to 0x01000FFF (4KByte memory)
     (*rom_style = "block" *) reg [31:0] imem [10'h000/4:10'hFFC/4];
     
@@ -19,10 +17,7 @@ module instruction_mem(
     always@(posedge clk)
     begin
         //memory is byte addressed, however only entire word can be accessed at a time
-        if(rd) instr_temp <= imem[instr_addr[11:2]];
+        if(rd) instr <= imem[instr_addr[11:2]];
     end
-    
-    //send ECALL HALT instruction if address is out of bounds
-    assign instr = (instr_addr[31:12]==20'h01000) ? instr_temp : { {25{1'b0}} , `SYSTEM };
     
 endmodule
