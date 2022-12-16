c254fc9c7543fd8ec05cf99536f729edcf419894
Vidyut <40513877+catchyviddy@users.noreply.github.com>
2022-12-16 00:47:13 -0500

    Update program_counter_tb.v

--- testbench/program_counter_tb.v
+++ testbench/program_counter_tb.v
@@ -2,21 +2,27 @@
 
 module program_counter_tb();
 
-reg clk,rst,imm;
+reg clk,rstn,imm,we;
 reg [31:0] imm_addr;
 wire [31:0] instr_addr;
+wire halt;
 
 program_counter PC (
         .clk(clk),
-        .rst(rst),
+        .rstn(rstn),
         .imm(imm),
+        .we(we),
         .imm_addr(imm_addr),
-        .instr_addr(instr_addr)
+        .instr_addr(instr_addr),
+        .halt(halt)
 );
 
-reg rst_f,imm_f;
+reg rstn_f,imm_f,we_f,halt_f;
 reg [31:0] imm_addr_f, instr_addr_f;
 integer file_test_cases;
+
+parameter clock_period=15;
+always #(clock_period/2) clk=~clk;
 
 initial begin: PROGRAM_COUNTER
 
@@ -31,23 +37,26 @@ initial begin: PROGRAM_COUNTER
 
     while( !$feof(file_test_cases)) begin
         
-        if (instr_addr !== instr_addr_f) begin
-            $display("Output does not match expected value.");
+        if ((instr_addr !== instr_addr_f) && (instr_addr_f !== 32'bx)) begin
+            $display("Instruction address does not match expected value.");
             $stop;
-        end     
+        end  
         
-        #5;    
+        if ((halt !== halt_f) && (halt_f !== 1'bx)) begin
+            $display("Halt signal does not match expected value.");
+            $stop;
+        end   
+        
+        #(clock_period/4);    
  
-        $fscanf(file_test_cases,"%b,%b,%h,%h",rst_f,imm_f,imm_addr_f,instr_addr_f);
+        $fscanf(file_test_cases,"%b,%b,%b,%h,%h,%b",rstn_f,we_f,imm_f,imm_addr_f,instr_addr_f,halt_f);
  
-        rst = rst_f;   
-        imm = imm_f;     
+        rstn = rstn_f;   
+        imm = imm_f; 
+        we = we_f;    
         imm_addr = imm_addr_f;     
 
-        #5;
-        clk = 1;
-        #10;
-        clk = 0;        
+        #(clock_period*3/4);          
              
     end
     
