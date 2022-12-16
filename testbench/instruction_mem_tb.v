75d0c81fec0322e213e45dde14811e323d31affb
Vidyut <40513877+catchyviddy@users.noreply.github.com>
2022-12-16 01:15:14 -0500

    Update instruction_mem_tb.v

--- testbench/instruction_mem_tb.v
+++ testbench/instruction_mem_tb.v
@@ -1,24 +1,31 @@
-`timescale 1ps / 0.1ps
+`timescale 1ns / 1ps
 
 module instruction_mem_tb();
 
-reg clk;
+reg clk, rd;
 reg [31:0] instr_addr;
 wire [31:0] instr;
 
 instruction_mem imem1 (
         .clk(clk),
+        .rd(rd),
         .instr_addr(instr_addr),
         .instr(instr)
 );
 
+reg rd_f;
 reg [31:0] instr_addr_f;
 reg [31:0] instr_f;
 
 integer file_test_cases;
 
+parameter clock_period=15;
+always #(clock_period/2) clk=~clk;
+
 initial begin: INSTRUCTION_MEMORY
 
+   $readmemh("imem_tb.mem", imem1.imem);
+   
    file_test_cases = $fopen("imem_testcases.csv","r");
     if (file_test_cases == 0) begin
         $display("Could not open test cases file.");
@@ -34,16 +41,15 @@ initial begin: INSTRUCTION_MEMORY
             $display("Output does not match expected value.");
             $stop;
         end     
+         
+        #(clock_period/4);
         
-        #5;    
- 
-        $fscanf(file_test_cases,"%h,%h",instr_addr_f,instr_f);
+        $fscanf(file_test_cases,"%b,%h,%h",rd_f,instr_addr_f,instr_f);
+        
+        rd = rd_f;
         instr_addr = instr_addr_f;     
 
-        #5;
-        clk = 1;
-        #10;
-        clk = 0;        
+        #(clock_period*3/4);               
              
     end
     
