c62b163c759f12aaaf88e8df81a565f1b0f8360b
Vidyut <40513877+catchyviddy@users.noreply.github.com>
2022-12-16 22:47:06 -0500

    Update top_riscv_wrapper_int_tb.v

--- testbench/top_riscv_wrapper_int_tb.v
+++ testbench/top_riscv_wrapper_int_tb.v
@@ -29,33 +29,60 @@ wire [3:0] an;
 
 top_riscv_wrapper DUT(.clk(clk), .btnU(rst), .sw(sw), .led(led), .seg(seg), .an(an));
 
+reg rst_f;
+reg [15:0] sw_f, led_f;
+reg [3:0] an_f;
+
+integer file_test_cases;
+integer cycles;
+
 parameter clock_period=15;
 always #(clock_period/2) clk=~clk;
 
-initial begin: INTEGRATION_TESTS
-    
+initial begin: INTEGRATION_TESTS 
+   
+    file_test_cases = $fopen("integration_testcases.csv","r");
+    if (file_test_cases == 0) begin
+        $display("Could not open test cases file.");
+        $stop;
+    end
+    $display("Opened test cases file.");
+        
     clk = 0;
     rst = 1;
-    sw = 16'h070a;
-    #(clock_period*10);    
-
-    rst = 0;
-    #(clock_period*200);
+    #(clock_period*10);
     
-    rst = 1;
-    sw = 16'h000a;
-    #(clock_period*10);    
+    led_f = 16'h0000;
+    an_f = 4'h0;
 
-    rst = 0;
-    #(clock_period*200);
-    
-    rst = 1;
-    sw = 16'h0007;
-    #(clock_period*10);    
+    while( !$feof(file_test_cases)) begin
+        
+        // MEM related signals
+        
+        if (led !== led_f) begin
+            $display("LED output %h does not match expected value %h at %0t ps.",led,led_f,$time);
+            $stop;
+        end 
 
-    rst = 0;
-    #(clock_period*200);
+        // HALT related signals
+        
+        if ((seg !== 7'h00) || (an !== an_f)) begin
+            $display("HALT output %h does not match expected value %h at %0t ps.",an,an_f,$time);
+            $stop;
+        end
+                
+        $fscanf(file_test_cases,"%b,%h,%h,%h,%d",rst_f,sw_f,led_f,an_f,cycles);
+        
+        rst = rst_f;
+        sw = sw_f;
+        
+        #(clock_period*cycles);
+     
+    end
     
+    $fclose(file_test_cases);
+    $display("All testcases passed!");
+ 
     $finish;
 
 end
